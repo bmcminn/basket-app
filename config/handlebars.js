fdec5f512494d8b577da72b3134804747fac2b2f
bmcminn <bmcminn@users.noreply.github.com>
2016-08-11 03:27:21 -0500

    adding props method to handlebars instance to get dynamic key value of collection

--- config/handlebars.js
+++ config/handlebars.js
@@ -9,17 +9,33 @@ module.exports = {
          */
         json:   function(context) { return JSON.stringify(context); }
 
+
+        /**
+         * Returns the value of a collection key given a dynamic property name
+         * @param  {collection}     context [description]
+         * @param  {string}         context [description]
+         * @return {string}         [description]
+         */
+    ,   prop:   function(context, value) {
+            return context[value];
+        }
+
         /**
          * Ifval allows Handlebars to use equality checks in templates
          * @param  {object/property}    conditional The model property we wish to check for a given set of values
-         * @param  {string/int}         options     The comparator(s) used for the evaluation
+         * @param  {string/int}         opts     The comparator(s) used for the evaluation
          * @return {string}             Returns given condition block results
          */
-    ,   ifval:  function (conditional, options) {
-            if (options.hash.value === conditional) {
-                return options.fn(this);
+    ,   ifval:  function (conditional, opts) {
+            console.log(conditional);
+            console.log(opts.hash);
+
+            console.log(opts.fn(this));
+
+            if (opts.hash.value === conditional) {
+                return opts.fn(this);
             } else {
-                return options.inverse(this);
+                return opts.inverse(this);
             }
         }
     }
